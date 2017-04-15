276914339e78852ba7b7fb6cb731bc8ab7b926cb
Kostyshyn <kostyshyn@ua.fm>
2017-04-16 01:00:19 +0300

    beta 0.3

--- app/js/index.js
+++ app/js/index.js
@@ -2,6 +2,7 @@ var openButton = document.querySelector('.open_button');
 var hiddenMenu = document.querySelector('.menu');
 var container = document.querySelector('.container');
 var dimmed = document.querySelector('.dimm');
+var fixedMenu = document.querySelector('.fixed_menu') || null;
 
 // Navbar animation 
 
@@ -9,3 +10,14 @@ openButton.addEventListener('click', function(){
 	hiddenMenu.classList.toggle('hidden');
 	dimmed.classList.toggle('dimmed');
 });
+
+if (fixedMenu){
+	document.addEventListener('scroll', function(){
+		var scroll = window.pageYOffset;
+		if (scroll > 580 && window.innerWidth > 780){
+			fixedMenu.classList.remove('hidden_top');
+		} else if (scroll < 80 && window.innerWidth > 780){
+			fixedMenu.classList.add('hidden_top');
+		}
+	});
+}
