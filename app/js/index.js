f417f7b026368714ecea82c026c462a4e6d4d67b
Kostyshyn <kostyshyn@ua.fm>
2017-04-25 19:19:07 +0300

    beta 0.9 lead_form fix

--- app/js/index.js
+++ app/js/index.js
@@ -20,15 +20,8 @@ if (fixedMenu){
 			fixedMenu.classList.add('hidden_top');
 		}
 	});
-	}
-	
-// Bootstrap calendar
+}
 
- $(function () {
-$('#datetimepicker1').datetimepicker({language: 'uk',
- minDate:new Date(),
-minuteStepping:10});
-$('#datetimepicker2').datetimepicker({language: 'uk',
-minDate: new Date(),
-minuteStepping:10,});
-});
+// Date picker
+
+$(".datepicker").datepicker();
